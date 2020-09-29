46f5839edfa8a0bc281b203aa17b962dd41d15c8
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2020-09-29 19:58:25 +0530

    Update H.java

--- Patterns/H.java
+++ Patterns/H.java
@@ -1,12 +1,12 @@
 
 import java.util.Scanner;
 
-//	*  *  *    *  *  * 0  for part 1 n=3			for part 3
-//	*  *          *  * 1	i  j1	sp	j2			i	j1	sp	j2
-//	*                * 2	0	3	0	3			0	1	12	1
-//					   0	1	2	6	2			1	2	6	2
-//					   1	2	1	12	1			2	3	0	3
-//	*                * 0  for part 2 print (n-1)
+//	*  *  *    *  *  * 0  for part 1 n=3					for part 3
+//	*  *          *  * 1	i  	j1	sp  	j2			i	j1	sp	j2	
+//	*                * 2	0   	3	0    	3			0	1	12	1
+//				1   	2	6    	2			1	2	6	2
+//				2   	1	12   	1			2	3	0	3
+//	*                * 0  for part 2 print (n-1) 
 //	*  *          *  * 1  blank lines...
 //	*  *  *    *  *  * 2
 
@@ -32,7 +32,7 @@ public class H {
 			System.out.println();
 
 
-		for(int i=0;i<n;i++)
+		for(int i=0;i<n;i++)//part3
 		{
 			for(int j1=0;j1<i+1;j1++)
 				System.out.print("*  ");
