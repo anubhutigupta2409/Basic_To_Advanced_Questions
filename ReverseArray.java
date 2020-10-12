d8e84185dc4f96982631dd12306cc2e8a2198b63
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2020-10-12 23:28:58 +0530

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,11 +1,11 @@
 //reversing an array without using another temporary array
 /*
 1 2 3 4 5 6	Change- 5 -> 0
-6 5 4 3 2 1			4 -> 1
-					3 -> 2
-0 1 2 3 4 5			2 -> 3
-					1 -> 4
-					0 -> 5 (i -> n-1-i)
+6 5 4 3 2 1		4 -> 1
+			3 -> 2
+0 1 2 3 4 5		2 -> 3
+			1 -> 4
+			0 -> 5 (i -> n-1-i)
 */
 
 import java.util.*;
