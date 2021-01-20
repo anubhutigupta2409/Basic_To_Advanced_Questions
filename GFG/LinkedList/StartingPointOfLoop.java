89ac6bff6e713445f1a7bee34ecdac59fedf6245
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2021-01-20 10:59:11 +0530

    Add files via upload

---
+++ GFG/LinkedList/StartingPointOfLoop.java
@@ -0,0 +1,36 @@
+/*
+Write a function findFirstLoopNode() that checks whether a given Linked List contains a loop. If the loop is present then it
+returns point to the first node of the loop. Else it returns NULL.
+*/
+
+/*
+approach--> perform floyd's cycle algorithm to detect the loop, if not found return null else-->
+1. set the byOne pointer equal to head and let the byTwo pointer be at it's position only
+2. now start making the pointers shift one node further ,the point at which they are equal is the starting of the node
+3. return the pointer
+*/
+
+class StartingPointOfLoop
+{
+	Node findFirstLoopNode(Node head)
+	{
+		Node byOne = head, byTwo= head;
+		while(byOne!=null && byTwo!=null && byTwo.next!=null)
+		{
+			if(byOne==byTwo)
+				break;
+
+			byOne=byOne.next;
+			byTwo=byTwo.next.next;
+		}
+		if(byOne!=byTwo)
+			return null;
+		byOne = head;
+		while(byOne!=byTwo)
+		{
+			byOne=byOne.next;
+			byTwo=byTwo.next;
+		}
+		return byOne;
+	}
+}
