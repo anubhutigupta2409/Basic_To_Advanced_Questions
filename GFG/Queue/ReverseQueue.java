7be95d6092be4fcefe970025754e27e554577878
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2021-04-29 16:44:24 +0530

    Add files via upload

---
+++ GFG/Queue/ReverseQueue.java
@@ -0,0 +1,58 @@
+/*
+
+Given a Queue Q containing N elements. The task is to reverse the Queue. Your task is to complete the function rev(), that reverses the N elements of the queue.
+
+Example 1:
+
+Input:
+6
+4 3 1 10 2 6
+
+Output:
+6 2 10 1 3 4
+
+Explanation:
+After reversing the given
+elements of the queue , the resultant
+queue will be 6 2 10 1 3 4.
+Example 2:
+
+Input:
+4
+4 3 2 1
+
+Output:
+1 2 3 4
+
+Explanation:
+After reversing the given
+elements of the queue , the resultant
+queue will be 1 2 3 4.
+Your Task:
+ You only need to complete the function rev that takes a queue as parameter and returns the reversed queue. The printing is done automatically by the driver code.
+
+Expected Time Complexity : O(n)
+Expected Auxilliary Space : O(n)
+
+Constraints:
+1 = N = 105
+1 = elements of Queue = 105
+*/
+
+class ReverseQueue{
+    //Function to reverse the queue.
+
+    public Queue<Integer> rev(Queue<Integer> q){
+        //add code here.
+
+        Stack<Integer> stk = new Stack<Integer>();
+
+        while(!q.isEmpty())
+            stk.push(q.poll());
+
+        while(!stk.empty())
+            q.add(stk.pop());
+
+        return q;
+    }
+}
