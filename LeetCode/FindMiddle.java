0ff49ce8a6b2ab57feb7ce0b01dfb0254d07314a
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2021-05-10 22:07:04 +0530

    Add files via upload

---
+++ LeetCode/FindMiddle.java
@@ -0,0 +1,76 @@
+/*
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+
+
+
+Example 1:
+
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+Example 2:
+
+Input: [1,2,3,4,5,6]
+Output: Node 4 from this list (Serialization: [4,5,6])
+Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+
+Note:
+
+The number of nodes in the given list will be between 1 and 100.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+ /*
+ Approach-->
+
+ Odd number of elements
+
+ 1 -> 2 -> 3 -> 4 -> 5		1 -> 2 -> 3 -> 4 -> 5		1 -> 2 -> 3 -> 4 -> 5
+ f									  f										f
+ s								 s								  s
+
+ fast.next==null 	->		return slow
+
+ Even number of elements
+
+ 1 -> 2 -> 3 -> 4 -> 5 -> 6		1 -> 2 -> 3 -> 4 -> 5 -> 6		1 -> 2 -> 3 -> 4 -> 5 -> 6		1 -> 2 -> 3 -> 4 -> 5 -> 6
+ f										  f											f										f
+ s									 s									  s									   s
+
+ fast==null		->		return slow
+ */
+class FindMiddle {
+    public ListNode middleNode(ListNode head) {
+
+        if(head == null)
+            return head;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null)
+        {
+            fast= fast.next.next;
+            slow= slow.next;
+        }
+
+        return slow;
+
+    }
+}
