4ffff2954ae90ebe1c7a7776b85b46bf3023bc80
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2021-05-11 19:47:30 +0530

    Add files via upload

---
+++ GFG/LinkedList/ReverseDLL.java
@@ -0,0 +1,55 @@
+/*
+
+Given a doubly linked list of n elements. The task is to reverse the doubly linked list.
+
+Example 1:
+
+Input:
+LinkedList: 3 <--> 4 <--> 5
+Output: 5 4 3
+Example 2:
+
+Input:
+LinkedList: 75 <--> 122 <--> 59 <--> 196
+Output: 196 59 122 75
+Your Task:
+Your task is to complete the given function reverseDLL(), which takes head reference as argument and should reverse the elements
+so that the tail becomes the new head and all pointers are correctly pointed. You need to return the new head of the reversed
+list. The printing and verification is done by the driver code.
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= number of nodes <= 103
+0 <= value of nodes <= 103
+*/
+
+public class ReverseDLL
+{
+public static Node reverseDLL(Node  head)
+{
+    //Your code here
+    if(head==null)
+        return head;
+
+    Node temp=null;
+    Node curr=head;
+
+    while(curr!=null)
+    {
+        //swap the previous and next pointers of each node
+        temp=curr.prev;
+        curr.prev=curr.next;
+        curr.next=temp;
+
+        //as now to move forward we need to move to the previous node
+        curr=curr.prev;
+    }
+
+    if(temp!=null)
+        head=temp.prev;//as temp is the second last node of the list
+
+    return head;
+}
+}
