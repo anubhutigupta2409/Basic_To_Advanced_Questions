d00b9122b1d9468dbd04720d8176b3a79bc01a59
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2021-05-13 10:33:55 +0530

    Add files via upload

---
+++ GFG/LinkedList/FlattenLL.java
@@ -0,0 +1,104 @@
+/*
+
+Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
+(i) a next pointer to the next node,
+(ii) a bottom pointer to a linked list where this node is head.
+Each of the sub-linked-list is in sorted order.
+Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order.
+Note: The flattened list will be printed using the bottom pointer instead of next pointer.
+
+
+
+Example 1:
+
+Input:
+5 -> 10 -> 19 -> 28
+|     |     |     |
+7     20    22   35
+|           |     |
+8          50    40
+|                 |
+30               45
+Output:  5-> 7-> 8- > 10 -> 19-> 20->
+22-> 28-> 30-> 35-> 40-> 45-> 50.
+Explanation:
+The resultant linked lists has every
+node in a single level.
+(Note: | represents the bottom pointer.)
+
+
+Example 2:
+
+Input:
+5 -> 10 -> 19 -> 28
+|          |
+7          22
+|          |
+8          50
+|
+30
+Output: 5->7->8->10->19->20->22->30->50
+Explanation:
+The resultant linked lists has every
+node in a single level.
+
+(Note: | represents the bottom pointer.)
+
+
+Your Task:
+You do not need to read input or print anything. Complete the function flatten() that takes the head of the linked list as input
+parameter and returns the head of flattened link list.
+
+
+
+Expected Time Complexity: O(N*M)
+Expected Auxiliary Space: O(1)
+
+
+
+Constraints:
+0 <= N <= 50
+1 <= Mi <= 20
+1 <= Element of linked list <= 103
+*/
+
+class FlattenLL
+{
+    Node merge(Node a, Node b)
+    {
+        if(a==null)
+            return b;
+        if(b==null)
+            return a;
+
+        Node res;
+
+        if(a.data<b.data)
+        {
+            res=a;
+            res.bottom= merge(a.bottom, b);//since bottom lists are sorted
+        }
+        else
+        {
+            res=b;
+            res.bottom=merge(a,b.bottom);
+        }
+        res.next=null;//we had to link flattened list using bottom pointer
+
+        return res;
+    }
+    Node flatten(Node root)
+    {
+	// Your code here
+	    if(root==null || root.next==null)
+	        return root;
+
+	   //recursive leap of faith, this return a flatten right sub-list
+	   root.next=flatten(root.next);
+	   //now we just have to merge the root list and it's already flattened
+	   //right sub-list
+	   root=merge(root,root.next);
+
+	   return root;
+    }
+}
