2df439bbcde3778a736db83195d695b8ad3ba7fc
ANUBHUTI GUPTA <56643076+anubhutigupta2409@users.noreply.github.com>
2021-08-05 11:34:15 +0530

    Revisiting Arrays

---
+++ LeetCode/Arrays/RotateArrayKStepsToTheRight_2.java
@@ -0,0 +1,30 @@
+/*
+Amazing mathematical explanation for the same over here -->
+
+https://leetcode.com/problems/rotate-array/discuss/54250/Easy-to-read-Java-solution/55849
+
+*/
+
+class RotateArrayKStepsToTheRight_2 {
+    private void reverse(int nums[], int start, int end)
+    {
+        while(start<end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+
+        int n= nums.length;
+        k%=n;
+
+        reverse(nums, 0, n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+}
