b76bd08fc9217cf779a99164bf9cde1544059eb2
ANUBHUTI GUPTA <56643076+anubhutigupta2409@users.noreply.github.com>
2021-09-02 18:04:55 +0530

    Revisiting LinkedList

--- LeetCode/LinkedList/IntersectionOfA_YList.java
+++ LeetCode/LinkedList/IntersectionOfA_YList.java
@@ -205,7 +205,11 @@ B:     b1 ? b2 ? b3 ? null
                       a = null
 Since a == b is true (both refer to null), end loop while(a != b), return a = null.
 
-Notice that if list A and list B have the same length, this solution will terminate in no more than 1 traversal; if both lists have different lengths, this solution will terminate in no more than 2 traversals -- in the second traversal, swapping a and b synchronizes a and b before the end of the second traversal. By synchronizing a and b I mean both have the same remaining steps in the second traversal so that it's guaranteed for them to reach the first intersection node, or reach null at the same time (technically speaking, in the same iteration) -- see Case 2 (Have Intersection & Different Len) and Case 4 (Have No Intersection & Different Len).
+Notice that if list A and list B have the same length, this solution will terminate in no more than 1 traversal; if both lists have different lengths, 
+this solution will terminate in no more than 2 traversals -- in the second traversal, swapping a and b synchronizes a and b before the end of the second 
+traversal. By synchronizing a and b I mean both have the same remaining steps in the second traversal so that it's guaranteed for them to reach the first 
+intersection node, or reach null at the same time (technically speaking, in the same iteration) -- see Case 2 (Have Intersection & Different Len) and
+Case 4 (Have No Intersection & Different Len).
 */
 
 public class IntersectionOfA_YList {
