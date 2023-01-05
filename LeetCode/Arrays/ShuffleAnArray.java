f78db53567eef767f0961904e0372b7f1289d20a
ANUBHUTI GUPTA <56643076+anubhutigupta2409@users.noreply.github.com>
2023-01-05 14:04:14 +0530

    Create ShuffleAnArray.java

---
+++ LeetCode/Arrays/ShuffleAnArray.java
@@ -0,0 +1,87 @@
+/*
+Shuffle an Array
+
+Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+Implement the Solution class:
+
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array.
+ 
+
+Example 1:
+
+Input
+["Solution", "shuffle", "reset", "shuffle"]
+[[[1, 2, 3]], [], [], []]
+Output
+[null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.shuffle();    // Shuffle the array [1,2,3] and return its result.
+                       // Any permutation of [1,2,3] must be equally likely to be returned.
+                       // Example: return [3, 1, 2]
+solution.reset();      // Resets the array back to its original configuration [1,2,3]. Return [1, 2, 3]
+solution.shuffle();    // Returns the random shuffling of array [1,2,3]. Example: return [1, 3, 2]
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+-106 <= nums[i] <= 106
+All the elements of nums are unique.
+At most 104 calls in total will be made to reset and shuffle.
+*/
+
+class Solution {
+
+    private int nums[];
+    private Random rand;
+
+    public Solution(int[] nums) {
+
+        this.nums = nums;
+        rand = new Random();
+        
+    }
+    
+    public int[] reset() {
+
+        return nums;
+        
+    }
+    
+    public int[] shuffle() {
+
+        if(nums==null)
+            return nums;//basic edge case
+
+        int arr[] = nums.clone();//since we need to retain the original array
+
+        for(int j=1;j<arr.length;j++)
+        {
+            int i = rand.nextInt(j+1);
+            swap(arr,i,j);
+        }
+
+        return arr;
+        
+    }
+
+    private void swap(int arr[],int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
